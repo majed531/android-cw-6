044bc86ccc54735e09bbe6ba62ac6f651ac78c5e
majed531 <66701995+majed531@users.noreply.github.com>
2020-07-01 17:56:04 +0300

    ss

---
+++ cw6/app/src/main/java/com/example/cw6/movie.java
@@ -0,0 +1,12 @@
+package com.example.cw6;
+
+
+
+public class movie {
+   private String movie ;
+    private String  mainactor;
+     private double Movierate;
+     private int pgrate;
+     private String genre;
+    }
+
